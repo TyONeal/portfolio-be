9489389431efda210d9acff516e19175d960c189
Ty O'Neal <ty.mcdaniel.oneal@gmail.com>
2023-11-16 22:45:43 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Hi there! 
+
+This is the back-end of my decoupled portfolio.
+
+Here we're using Java with the Spring Boot framework!
---
+++ src/test/java/com/tyoneal/portfoliobe/PortfolioBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyoneal.portfoliobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
